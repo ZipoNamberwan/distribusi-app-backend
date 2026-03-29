d1886105ed716d34be9c5026741d9ade02a5d9d7
zipo <dopokan@gmail.com>
2026-03-29 12:54:08 +0000

    refactor: remove empty fetch event listener from service worker

--- public/sw.js
+++ public/sw.js
@@ -4,8 +4,3 @@ self.addEventListener('install', (e) => {
     // Force the waiting service worker to become the active service worker
     self.skipWaiting();
 });
-
-self.addEventListener('fetch', (e) => {
-    // Chrome requires a fetch event handler for a valid PWA.
-    // We don't need any offline caching strategy here, just a pass-through.
-});
